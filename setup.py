da1d53a5e892a6e00f81cf41416bc4239d87f887
XTao <xutao881001@gmail.com>
2014-07-06 22:59:13 +0800

    Update setup link.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='Sina',
     version='0.1',
-    url='http://code.dapps.douban.com/xutao/sina',
+    url='http://github.com/douban/sina',
     license='New BSD',
     author='xutao',
     author_email='xutao@douban.com',
