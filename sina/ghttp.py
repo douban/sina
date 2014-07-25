1bbe550745d61ceae5474666053926cd93401676
XTao <xutao881001@gmail.com>
2014-07-25 13:19:51 +0800

    Add request.

--- sina/ghttp.py
+++ sina/ghttp.py
@@ -80,55 +80,60 @@ class GHTTPServer(object):
     def __call__(self, environ, start_response):
         if hasattr(self, '_before_request_handler'):
             self._before_request_handler(environ)
-        self.headers = {}
-        self.env = environ
-        body = self.call()
-        start_response(self.status, self.headers.items())
+        request = Request(environ=environ)
+        body = self.call(request)
+        start_response(request.status, request.headers.items())
         if hasattr(self, '_after_request_handler'):
             self._after_request_handler(environ)
         return body
 
-    def call(self):
-        match = self.match_routing(self.env["PATH_INFO"].lstrip('/'),
-                                   self.env["REQUEST_METHOD"])
+    def call(self, request):
+        match = self.match_routing(request.environ["PATH_INFO"].lstrip('/'),
+                                   request.environ["REQUEST_METHOD"])
         if not match:
-            return self.render_not_found()
+            return self.render_not_found(request)
         cmd, path, reqfile, rpc = match
-        self.rpc = rpc
-        self.reqfile = reqfile
+        request.rpc = rpc
+        request.reqfile = reqfile
         if cmd == "not_allowed":
-            return self.render_method_not_allowed()
+            return self.render_method_not_allowed(request)
 
         if hasattr(self, '_has_permission_handler'):
-            need_perm = self.get_permission(cmd, rpc)
-            has_perm = self._has_permission_handler(self.env, path, need_perm)
+            need_perm = self.get_permission(cmd, rpc, request.environ['QUERY_STRING'])
+            has_perm = self._has_permission_handler(request.environ, path, need_perm)
             if not has_perm:
-                return self.render_no_access()
+                return self.render_no_access(request)
 
-        self.dir = self.get_git_dir(path)
-        if not self.dir:
-            return self.render_not_found()
+        if hasattr(self, '_get_repo_path_handler'):
+            request.dir = self._get_repo_path_handler(request.environ, path)
+        else:
+            request.dir = self.get_git_dir(path)
+
+        if not request.dir:
+            return self.render_not_found(request)
         func = getattr(self, cmd)
-        return func()
+        return func(request)
 
-    def service_rpc(self):
-        if not self.has_access(self.rpc, True):
-            return self.render_no_access()
-        input = self.read_body
-        git_cmd = "upload_pack" if self.rpc == "upload-pack" else "receive_pack"
-        self.status = "200 OK"
-        self.headers["Content-Type"] = "application/x-git-%s-result" % self.rpc
-        env = self.env.get('env')
-        return getattr(self.git, git_cmd)(self.dir, {"msg": input, "env": env}, callback)
+    def service_rpc(self, request):
+        if not self.has_access(request, True):
+            return self.render_no_access(request)
+        input = self.read_body(request)
+        rpc = request.rpc
+        git_cmd = "upload_pack" if rpc == "upload-pack" else "receive_pack"
+        request.status = "200 OK"
+        request.headers["Content-Type"] = "application/x-git-%s-result" % rpc
+        env = request.environ.get('env')
+        return getattr(self.git, git_cmd)(request.dir, {"msg": input, "env": env}, callback)
 
-    def get_info_refs(self):
-        service_name = self.get_service_type()
-        if self.has_access(service_name):
+    def get_info_refs(self, request):
+        service_name = self.get_service_type(request.environ["QUERY_STRING"])
+        request.rpc = service_name
+        if self.has_access(request):
             git_cmd = "upload_pack" if service_name == "upload-pack" else "receive_pack"
-            refs = getattr(self.git, git_cmd)(self.dir, {"advertise_refs": True})
-            self.status = "200 OK"
-            self.headers["Content-Type"] = "application/x-git-%s-advertisement" % service_name
-            self.hdr_nocache()
+            refs = getattr(self.git, git_cmd)(request.dir, {"advertise_refs": True})
+            request.status = "200 OK"
+            request.headers["Content-Type"] = "application/x-git-%s-advertisement" % service_name
+            self.hdr_nocache(request)
 
             def read_file():
                 yield self.pkt_write("# service=git-%s\n" % service_name)
@@ -136,31 +141,31 @@ class GHTTPServer(object):
                 yield refs
             return read_file()
         else:
-            return self.dumb_info_refs()
+            return self.dumb_info_refs(request)
 
-    def get_text_file(self):
-        return self.send_file(self.reqfile, "text/plain")
+    def get_text_file(self, request):
+        return self.send_file(request, "text/plain")
 
-    def dumb_info_refs(self):
-        self.update_server_info()
-        return self.send_file(self.reqfile, "text/plain; charset=utf-8")
+    def dumb_info_refs(self, request):
+        self.update_server_info(request.dir)
+        return self.send_file(request, "text/plain; charset=utf-8")
 
-    def get_info_packs(self):
+    def get_info_packs(self, request):
         # objects/info/packs
-        return self.send_file(self.reqfile, "text/plain; charset=utf-8")
+        return self.send_file(request, "text/plain; charset=utf-8")
 
-    def get_loose_object(self):
-        return self.send_file(self.reqfile, "application/x-git-loose-object", cached=True)
+    def get_loose_object(self, request):
+        return self.send_file(request, "application/x-git-loose-object", cached=True)
 
-    def get_pack_file(self):
-        return self.send_file(self.reqfile, "application/x-git-packed-objects", cached=True)
+    def get_pack_file(self, request):
+        return self.send_file(request, "application/x-git-packed-objects", cached=True)
 
-    def get_idx_file(self):
-        return self.send_file(self.reqfile, "application/x-git-packed-objects-toc", cached=True)
+    def get_idx_file(self, request):
+        return self.send_file(request, "application/x-git-packed-objects-toc", cached=True)
 
-    def get_service_type(self):
+    def get_service_type(self, query_string):
         def get_param():
-            for query in self.env["QUERY_STRING"].split('&'):
+            for query in query_string.split('&'):
                 param = tuple(query.split('='))
                 if param and param[0] == "service":
                     return param[1]
@@ -189,25 +194,25 @@ class GHTTPServer(object):
                 return [cmd, path, file, rpc]
         return None
 
-    def send_file(self, reqfile, content_type, cached=False):
-        reqfile = join(self.dir, reqfile)
-        if not self.is_subpath(reqfile, self.dir):
-            return self.render_no_access()
+    def send_file(self, request, content_type, cached=False):
+        reqfile = join(request.dir, request.reqfile)
+        if not self.is_subpath(reqfile, request.dir):
+            return self.render_no_access(request)
         if not exists(reqfile) or not access(reqfile, os.R_OK):
-            return self.render_not_found()
+            return self.render_not_found(request)
 
-        self.status = "200 OK"
-        self.headers["Content-Type"] = content_type
-        self.headers["Last-Modified"] = format_date_time(getmtime(reqfile))
+        request.status = "200 OK"
+        request.headers["Content-Type"] = content_type
+        request.headers["Last-Modified"] = format_date_time(getmtime(reqfile))
 
         if cached:
-            self.hdr_cache_forenver()
+            self.hdr_cache_forenver(request)
         else:
-            self.hdr_nocache()
+            self.hdr_nocache(request)
 
         size = getsize(reqfile)
         if size:
-            self.headers["Content-Length"] = size
+            request.headers["Content-Length"] = size
 
             def read_file():
                 with open(reqfile, "rb") as f:
@@ -220,16 +225,15 @@ class GHTTPServer(object):
         else:
             with open(reqfile, "rb") as f:
                 part = f.read()
-                self.headers["Content-Length"] = str(len(part))
+                request.headers["Content-Length"] = str(len(part))
             return [part]
 
-    def update_server_info(self):
-        self.git.update_server_info(self.dir)
+    def update_server_info(self, path):
+        self.git.update_server_info(path)
 
-    @property
-    def read_chunked_body(self):
+    def read_chunked_body(self, request):
         # wsgiref with no chunked support
-        environ = self.env
+        environ = request.environ
         input = environ.get('wsgi.input')
         length = environ.get('CONTENT_LENGTH', '0')
         length = 0 if length == '' else int(length)
@@ -247,12 +251,12 @@ class GHTTPServer(object):
             body = input.read(length)
         return body
 
-    @property
-    def read_body(self):
+    def read_body(self, request):
         if self.config.get('chunked'):
-            return self.read_chunked_body
-        input = self.env.get('wsgi.input')
-        if self.env.get('HTTP_CONTENT_ENCODING') == 'gzip':
+            return self.read_chunked_body(request)
+        env = request.environ
+        input = env.get('wsgi.input')
+        if env.get('HTTP_CONTENT_ENCODING') == 'gzip':
             compressedstream = StringIO.StringIO(input.read())
             gzipper = gzip.GzipFile(fileobj=compressedstream)
             return gzipper.read()
@@ -280,45 +284,47 @@ class GHTTPServer(object):
     # header writing functions
     # ------------------------
 
-    def hdr_nocache(self):
-        self.headers["Expires"] = "Fri, 01 Jan 1980 00:00:00 GMT"
-        self.headers["Pragma"] = "no-cache"
-        self.headers["Cache-Control"] = "no-cache, max-age=0, must-revalidate"
+    def hdr_nocache(self, request):
+        request.headers["Expires"] = "Fri, 01 Jan 1980 00:00:00 GMT"
+        request.headers["Pragma"] = "no-cache"
+        request.headers["Cache-Control"] = "no-cache, max-age=0, must-revalidate"
 
-    def hdr_cache_forenver(self):
+    def hdr_cache_forenver(self, request):
         now = int(time.time())
-        self.headers["Date"] = str(now)
-        self.headers["Expires"] = str(now + 31536000)
-        self.headers["Cache-Control"] = "public, max-age=31536000"
+        request.headers["Date"] = str(now)
+        request.headers["Expires"] = str(now + 31536000)
+        request.headers["Cache-Control"] = "public, max-age=31536000"
 
     # --------------------------------------
     # HTTP error response handling functions
     # --------------------------------------
 
-    def render_method_not_allowed(self):
-        env = []
+    def render_method_not_allowed(self, request):
+        env = request.environ
         if env["SERVER_PROTOCOL"] == "HTTP/1.1":
-            self.status = "405 Method not allowed"
-            self.headers["Content-Type"] = "text/plain"
+            request.status = "405 Method not allowed"
+            request.headers["Content-Type"] = "text/plain"
             return ["Method Not Allowed"]
         else:
-            self.status = "400 Bad Request"
-            self.headers["Content-Type"] = "text/plain"
+            request.status = "400 Bad Request"
+            request.headers["Content-Type"] = "text/plain"
             return ["Bad Request"]
 
-    def render_not_found(self):
-        self.status = "404 Not Found"
-        self.headers["Content-Type"] = "text/plain"
+    def render_not_found(self, request):
+        request.status = "404 Not Found"
+        request.headers["Content-Type"] = "text/plain"
         return ["Not Found"]
 
-    def render_no_access(self):
-        self.status = "403 Forbidden"
-        self.headers["Content-Type"] = "text/plain"
+    def render_no_access(self, request):
+        request.status = "403 Forbidden"
+        request.headers["Content-Type"] = "text/plain"
         return ["Forbidden"]
 
-    def has_access(self, rpc, check_content_type=False):
+    def has_access(self, request, check_content_type=False):
+        rpc = request.rpc
+        env = request.environ
         if check_content_type:
-            if self.env["CONTENT_TYPE"] != "application/x-git-%s-request" % rpc:
+            if env["CONTENT_TYPE"] != "application/x-git-%s-request" % rpc:
                 return False
         if rpc not in self.VALID_SERVICE_TYPES:
             return False
@@ -328,11 +334,11 @@ class GHTTPServer(object):
         if rpc == 'upload-pack':
             if "upload_pack" in self.config:
                 return self.config.get("upload_pack")
-        return self.get_config_setting(rpc)
+        return self.get_config_setting(request.dir, rpc)
 
-    def get_config_setting(self, service_name):
+    def get_config_setting(self, path, service_name):
         service_name = service_name.replace('-', '')
-        setting = self.git.get_config_setting(self.dir,
+        setting = self.git.get_config_setting(path,
                                               "http.%s" % service_name)
         if service_name == 'uploadpack':
             return setting != 'false'
@@ -340,8 +346,6 @@ class GHTTPServer(object):
             return setting == 'true'
 
     def get_git_dir(self, path):
-        if hasattr(self, '_get_repo_path_handler'):
-            return self._get_repo_path_handler(self.env, path)
         root = self.get_project_root()
         path = join(root, path)
         if not self.is_subpath(path, root):
@@ -384,9 +388,20 @@ class GHTTPServer(object):
         self._has_permission_handler = f
         return f
 
-    def get_permission(self, cmd, rpc):
+    def get_permission(self, cmd, rpc, query):
         if cmd == 'get_info_refs':
-            rpc = self.get_service_type()
+            rpc = self.get_service_type(query)
         if rpc == 'receive-pack':
             return 'write'
         return 'read'
+
+
+class Request(object):
+
+    def __init__(self, headers=None, environ=None):
+        self.headers = {} if not headers else headers
+        self.environ = {} if not environ else environ
+        self.reqfile = None
+        self.rpc = None
+        self.status = None
+        self.dir = None
